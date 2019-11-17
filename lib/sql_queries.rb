11e243925fec911f39b3a8e523b03301038abac8
Alexander Bispo <alexbispo173@gmail.com>
2019-11-17 21:23:00 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,7 +18,7 @@ end
 
 def selects_oldest_bear_and_returns_name_and_age
   "
-  SELECT name,age FROM bears ORDER BY age DESC LIMIT 1;
+  SELECT name,age FROM bears ORDER BY age ASC LIMIT 1;
   "
 end
 
